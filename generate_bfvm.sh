be6213efbf682bbc0275e90afc6fd39af519b33b
Maxime Arthaud <maxime@arthaud.me>
2015-07-19 11:42:57 -0700

    Rename some variables

--- generate_bfvm.sh
+++ generate_bfvm.sh
@@ -12,47 +12,47 @@
 #
 ###############################################################################
 
-# [amovlw] move the cursor on the left (array write)
+# [a_movlw] move the cursor on the left (array write)
 # pre: the cursor is on i0
 # post: the cursor is on i0
-amovlw=">>>>>>>>>>[-<<<<<<<<<<<+>>>>>>>>>>>]
+a_movlw=">>>>>>>>>>[-<<<<<<<<<<<+>>>>>>>>>>>]
         <[->+<]<[->+<]<[->+<]<[->+<]
         <[->+<]<[->+<]<[->+<]<[->+<]<[->+<]<[->+<]
         >"
 
-# [amovrw] move the cursor on the right (array write)
+# [a_movrw] move the cursor on the right (array write)
 # pre: the cursor is on j0
 # post: the cursor is on j0
-amovrw="[-<+>]>[-<+>]>[-<+>]
+a_movrw="[-<+>]>[-<+>]>[-<+>]
         <<<<<<<[->>>>>>>>>>>+<<<<<<<<<<<]
         >>>>"
 
-# [awrite] store 4 bytes in the array
+# [a_write] store 4 bytes in the array
 # pre: s=0, [i0 i1 i2] = [j0 j1 j2] = index, [d0 d1 d2 d3] = value
 #      the cursor is on s
 # post: s=i0=i1=i2=j0=j1=j2=d0=d1=d2=d3=0
 #       the cursor is on s
-awrite=">[$amovlw-]
-        >[<-[$amovlw-] $amovlw >-]
-        >[<-[<-[$amovlw-] $amovlw >-] <$amovlw >>-]
+a_write=">[$a_movlw-]
+        >[<-[$a_movlw-] $a_movlw >-]
+        >[<-[<-[$a_movlw-] $a_movlw >-] <$a_movlw >>-]
         >>>>>>>>[-]>[-]>[-]>[-]<<<<<<<
         [->>>>+<<<<]>[->>>>+<<<<]>[->>>>+<<<<]>[->>>>+<<<<]<<<<<<
-        [$amovrw-]
-        >[<-[$amovrw-] $amovrw >-]
-        >[<-[<-[$amovrw-] $amovrw >-] <$amovrw >>-]
+        [$a_movrw-]
+        >[<-[$a_movrw-] $a_movrw >-]
+        >[<-[<-[$a_movrw-] $a_movrw >-] <$a_movrw >>-]
         <<<<<<"
 
-# [amovlr] move the cursor on the left (array read)
+# [a_movlr] move the cursor on the left (array read)
 # pre: the cursor is on i0
 # post: the cursor is on i0
-amovlr=">>>>>>>>>>[-<<<<<<<<<<<+>>>>>>>>>>>]
+a_movlr=">>>>>>>>>>[-<<<<<<<<<<<+>>>>>>>>>>>]
 	<<<<<[->+<]<[->+<]<[->+<]<[->+<]<[->+<]<[->+<]
         >"
 
-# [amovrr] move the cursor on the right (array read)
+# [a_movrr] move the cursor on the right (array read)
 # pre: the cursor is on j0
 # post: the cursor is on j0
-amovrr="[-<+>]>[-<+>]>[-<+>]>[-<+>]>[-<+>]>[-<+>]>[-<+>]
+a_movrr="[-<+>]>[-<+>]>[-<+>]>[-<+>]>[-<+>]>[-<+>]>[-<+>]
         <<<<<<<<<<<[->>>>>>>>>>>+<<<<<<<<<<<]
         >>>>"
 
@@ -61,14 +61,14 @@ amovrr="[-<+>]>[-<+>]>[-<+>]>[-<+>]>[-<+>]>[-<+>]>[-<+>]
 #      the cursor is on s
 # post: s=i0=i1=i2=j0=j1=j2=0, [d0 d1 d2 d3] = loaded value
 #       the cursor is on s
-aread=">[$amovlr-]
-       >[<-[$amovlr-] $amovlr >-]
-       >[<-[<-[$amovlr-] $amovlr >-] <$amovlr >>-]
+aread=">[$a_movlr-]
+       >[<-[$a_movlr-] $a_movlr >-]
+       >[<-[<-[$a_movlr-] $a_movlr >-] <$a_movlr >>-]
        >>>>>>>>[-<<<<+<<<<+>>>>>>>>]<<<<<<<<[->>>>>>>>+<<<<<<<<]
        >>>>>>>>>[-<<<<+<<<<<+>>>>>>>>>]<<<<<<<<<[->>>>>>>>>+<<<<<<<<<]
        >>>>>>>>>>[-<<<<+<<<<<<+>>>>>>>>>>]<<<<<<<<<<[->>>>>>>>>>+<<<<<<<<<<]
        >>>>>>>>>>>[-<<<<+<<<<<<<+>>>>>>>>>>>]<<<<<<<<<<<[->>>>>>>>>>>+<<<<<<<<<<<]>
-       [$amovrr-]
-       >[<-[$amovrr-] $amovrr >-]
-       >[<-[<-[$amovrr-] $amovrr >-] <$amovrr >>-]
+       [$a_movrr-]
+       >[<-[$a_movrr-] $a_movrr >-]
+       >[<-[<-[$a_movrr-] $a_movrr >-] <$a_movrr >>-]
        <<<<<<"
